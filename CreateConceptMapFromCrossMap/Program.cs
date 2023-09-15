25c42e4816a391087ad35d62dc8a9a1670459122
ajagann1 <arvind.jagannathan@lantanagroup.com>
2023-09-15 09:39:10 -0400

    Added in json conversion

--- CreateConceptMapFromCrossMap/Program.cs
+++ CreateConceptMapFromCrossMap/Program.cs
@@ -3,6 +3,10 @@ using Newtonsoft.Json.Linq;
 using DocumentFormat.OpenXml.Office.CustomUI;
 using Newtonsoft.Json;
 using System.Xml;
+using DocumentFormat.OpenXml.Math;
+using System.Text.Json;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Model;
 
 class Program
 {
@@ -10,22 +14,22 @@ class Program
     static void Main(String[] args)
     {
         //Cross Map (location is set to a direct filepath right now, modify however)
-        XLWorkbook mappings = new XLWorkbook("Input\\Yale New Haven Location Mappings.xlsx");
+        XLWorkbook mappings = new XLWorkbook("Input\\Encounter_Type_Mapping_URMC_2023-08-17.xlsx");
 
         XmlDocument doc = new XmlDocument();
         //Template file (ConceptMap with no mappings)
         doc.Load("Input\\ConceptMapTemplate.xml");
         XmlNode group = doc.GetElementsByTagName("group")[0];
 
-        IXLRow mapRow = mappings.Worksheet(1).Row(2);
+        IXLRow mapRow = mappings.Worksheet("Mappings").Row(2);
         while (!mapRow.IsEmpty())
         {
             //Client codes
-            String code = mapRow.Cell("D").Value.ToString().Trim();
+            String code = mapRow.Cell("A").Value.ToString().Trim();
             //Measure codes
-            String target = mapRow.Cell("Q").Value.ToString().Trim();
+            String target = mapRow.Cell("C").Value.ToString().Trim();
 
-            if (target != "")
+            if (target != "" && code != "")
             {
                 String tar = target.Contains("QM#LOC") ? target.Replace("QM#LOC", "") : target;
 
@@ -39,7 +43,7 @@ class Program
                 tRelationship.SetAttribute("value", "inexact");
 
                 XmlElement tDisplay = doc.CreateElement("display");
-                tDisplay.SetAttribute("value", mapRow.Cell("P").Value.ToString().Trim());
+                tDisplay.SetAttribute("value", mapRow.Cell("D").Value.ToString().Trim());
 
                 XmlElement t = doc.CreateElement("target");
                 t.AppendChild(tCode);
@@ -55,7 +59,32 @@ class Program
 
             mapRow = mapRow.RowBelow();
         }
+
+        var xmlStr = doc.OuterXml;
+        var deserializer = new FhirXmlPocoDeserializer(ModelInfo.ModelInspector, new FhirXmlPocoDeserializerSettings
+        {
+            Validator = null
+        });
+        Resource conceptMap = null;
+        Hl7.Fhir.Model.Base baseRes = null;
+        try
+        {
+            conceptMap = deserializer.DeserializeResource(xmlStr); //Deserialize<ConceptMap>(xmlStr);
+        }
+        catch (DeserializationFailedException ex)
+        {
+            baseRes = ex.PartialResult;
+        }
+        if (conceptMap == null)
+            conceptMap = (ConceptMap)baseRes;
+
+        var options = new JsonSerializerOptions().ForFhir(ModelInfo.ModelInspector).Pretty();
+        string conceptMapJson = System.Text.Json.JsonSerializer.Serialize(conceptMap, options);
+
         //Change output name to whatever you'd like
-        doc.Save("Output\\CompletedConceptMap.xml");
+        File.WriteAllText("Output\\CompletedConceptMap.json", conceptMapJson);
+
+        //Change output name to whatever you'd like
+        //doc.Save("Output\\CompletedConceptMap.xml");
     }
 }
